a77b2c9b16015afc84d665684e0949f95d2c54f2
Baryshev Alexandr <baryshev.alexandr89@gmail.com>
2019-04-28 21:01:57 +0300

    Updates

---
+++ precache-manifest.5cd2b8b04b66a66bbd3e9a035a1c1572.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8365fd7b118ba3028ae0c4261e3d4192",
+    "url": "/myapp/static/media/search.8365fd7b.svg"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  },
+  {
+    "revision": "71b86ca2ab5da37ee74e",
+    "url": "/myapp/static/js/main.a26ac1f0.chunk.js"
+  },
+  {
+    "revision": "041920a710a9828cb0b3",
+    "url": "/myapp/static/js/2.f3b9e38e.chunk.js"
+  },
+  {
+    "revision": "71b86ca2ab5da37ee74e",
+    "url": "/myapp/static/css/main.de6e1bdb.chunk.css"
+  },
+  {
+    "revision": "041920a710a9828cb0b3",
+    "url": "/myapp/static/css/2.cef4c66e.chunk.css"
+  },
+  {
+    "revision": "2b1e6a3ab1059a0885c9a2a3f496de4b",
+    "url": "/myapp/index.html"
+  }
+];
